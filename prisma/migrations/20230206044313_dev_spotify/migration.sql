cc197274ebef6bc21cd7fc53dfbbc14c873cf4cc
Shuvam <shuvamsantra10@gmail.com>
2023-02-06 11:48:09 +0530

    Prisma folder

---
+++ prisma/migrations/20230206044313_dev_spotify/migration.sql
@@ -0,0 +1,85 @@
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE', 'OTHER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "gender" "Gender",
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Playlist" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "image" TEXT,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Playlist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Song" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "artist" TEXT NOT NULL,
+    "album" TEXT NOT NULL,
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Song_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_PlaylistToSong" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_SongToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PlaylistToSong_AB_unique" ON "_PlaylistToSong"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PlaylistToSong_B_index" ON "_PlaylistToSong"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_SongToUser_AB_unique" ON "_SongToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_SongToUser_B_index" ON "_SongToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "Playlist" ADD CONSTRAINT "Playlist_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PlaylistToSong" ADD CONSTRAINT "_PlaylistToSong_A_fkey" FOREIGN KEY ("A") REFERENCES "Playlist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PlaylistToSong" ADD CONSTRAINT "_PlaylistToSong_B_fkey" FOREIGN KEY ("B") REFERENCES "Song"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_SongToUser" ADD CONSTRAINT "_SongToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Song"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_SongToUser" ADD CONSTRAINT "_SongToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/seed.js
@@ -0,0 +1,72 @@
+import { PrismaClient, Gender } from "@prisma/client";
+
+const db = new PrismaClient();
+
+async function seed() {
+  // create some users
+  const users = await Promise.all([
+    db.user.create({
+      data: {
+        name: "John Doe",
+        email: "johndoeexample.com",
+        password: "john@010",
+        gender: Gender.MALE,
+        image: "Image",
+        likedPlaylists: {
+          create: [
+            { title: "Johns favorite playlists", description: "Desc 1" },
+            { title: "Johns second favorite playlists", description: "Desc 2" },
+          ],
+        },
+        likedSongs: {
+          create: [
+            { title: "Johns favorite song", artist: "A", album: "A" },
+            { title: "Johns second favorite song", artist: "B", album: "A" },
+          ],
+        },
+      },
+    }),
+  ]);
+
+  // create some songs
+  const songs = await Promise.all([
+    db.song.create({
+      data: {
+        title: "Song 1",
+        artist: "Artist 1",
+        album: "A",
+        likedBy: {
+          connect: [{ id: users[0].id }],
+        },
+      },
+    }),
+  ]);
+
+  // create some playlists
+  const playlists = await Promise.all([
+    db.playlist.create({
+      data: {
+        title: "Playlist 1",
+        description: "PlayList",
+        songs: {
+          connect: [{ id: songs[0].id }],
+        },
+        userId: users[0].id,
+        image: "Image",
+      },
+    }),
+  ]);
+
+  console.log(`
+    Created ${users.length} users:
+    ${users.map((u) => u.name).join(", ")}
+
+    Created ${songs.length} songs:
+    ${songs.map((s) => s.title).join(", ")}
+
+    Created ${playlists.length} playlists:
+    ${playlists.map((p) => p.name).join(", ")}
+  `);
+}
+
+seed().catch((e) => console.error(e));
